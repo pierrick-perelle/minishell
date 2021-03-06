14a93cb91e7ca569ed84ba31714924a75bff4518
Pierrick Perelle <perellep@im2ag-mandelbrot.e.ujf-grenoble.fr>
2021-03-06 17:40:01 +0100

    multiple pipe

--- src/shell.c
+++ src/shell.c
@@ -14,6 +14,8 @@ int main()
 {
 	int status;
 
+	int old_fds[2], new_fds[2];
+
 	while (1) {
 		struct cmdline *l;
 		int i, j;
@@ -36,17 +38,52 @@ int main()
 		if (l->in) printf("in: %s\n", l->in);
 		if (l->out) printf("out: %s\n", l->out);
 
+		/* nbpipe */
+
+		/*int nbpipe = 0;
+		char** cur_cmd = l->seq[nbpipe];
+		
+		while(cur_cmd != NULL){
+			nbpipe++;
+			cur_cmd = l->seq[nbpipe];
+		}
+		nbpipe--;
+		printf("number of cmd : %i \n", nbpipe);*/
+
+
 		/* Display each command of the pipe */
+
 		for (i=0; l->seq[i]!=0; i++) {
 			char **cmd = l->seq[i];
 			printf("seq[%d]: ", i);
 			for (j=0; cmd[j]!=0; j++) {
 				printf("%s ", cmd[j]);
 			}
-			printf("\n");
-			
 
+			if(l->seq[i+1] != NULL){
+				/*there is a next command */
+				pipe(new_fds);
+			}
+
+			printf("\n");
 			if(Fork()==0){
+
+				/* Fils */
+
+				if(i > 0){
+					/*there is a previous command */
+					dup2(old_fds[0],0);
+					close(old_fds[0]);
+					close(old_fds[1]);
+				}
+
+				if(l->seq[i+1] != NULL){
+					/*there is a next command */
+					close(new_fds[0]);
+            		dup2(new_fds[1], 1);
+            		close(new_fds[1]);
+				}
+
 				if(l->out){
 					int fd_out = open(l->out, O_CREAT | O_WRONLY | O_TRUNC, 0744);
 					if (fd_out == -1){
@@ -72,12 +109,28 @@ int main()
 				}
 
 			}else{
+
+				/* Père */
+
+				if(i > 0){
+					/*there is a previous command */
+					close(old_fds[0]);
+            		close(old_fds[1]);
+				}
+
+				if(l->seq[i+1] != NULL){
+					/*there is a next command */
+					old_fds[0] = new_fds[0];
+					old_fds[1] = new_fds[1];
+				}
+
 				Wait(&status);
 			}
-
-			
-
-
+		}
+		if(i > 1){
+			/* multiple cmds */
+			close(old_fds[0]);
+    		close(old_fds[1]);
 		}
 	}
 }
